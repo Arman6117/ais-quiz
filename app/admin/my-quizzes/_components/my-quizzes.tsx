71afdb4380033ac2bc67be0046220dbf07dabce9
Arman Patel <vanixgaming634@gmail.com>
2026-02-09 12:31:20 +0530

    refactor

--- app/admin/my-quizzes/_components/my-quizzes.tsx
+++ app/admin/my-quizzes/_components/my-quizzes.tsx
@@ -62,11 +62,11 @@ const MyQuizzes = () => {
 
         {["all", "published", "draft", "archived"].map((filterStatus) => (
           <TabsContent key={filterStatus} value={filterStatus} className="mt-0 outline-none">
-            <div className="flex flex-wrap gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  lg:gap-16 gap-6">
               {mockQuizzes
                 .filter((quiz) => filterStatus === "all" || quiz.status === filterStatus)
                 .map((quiz) => (
-                  <QuizCard key={quiz.id} quiz={quiz} />
+                  <QuizCard  key={quiz.id} quiz={quiz} />
                 ))}
               
               {(filterStatus === "all" || filterStatus === "draft") && <CreateNewCard />}
